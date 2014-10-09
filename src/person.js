d5516615a392b8f0675057bf78d818e9190b5ff4
Ben Drucker <bvdrucker@gmail.com>
2014-10-09 13:28:46 -0400

    Allow a person to be fetched with Person#find

---
+++ src/person.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var assert = require('assert');
+var _      = require('lodash');
+
+module.exports = function (client) {
+  function Person (data) {
+    _.extend(this, data);
+  }
+
+  Person.find = function (options) {
+    assert(options && options.email, 'An email must be provided');
+    return this.client.request(_.extend({
+      api: 'person',
+      path: '/people/email/' + options.email,
+      query: _.pick(options, 'subscribe', 'company')
+    }, options))
+    .bind(this)
+    .then(function (data) {
+      return new this(data);
+    });
+  };
+
+  Person.prototype.client = Person.client = client;
+
+  return Person;
+};
---
+++ test/person.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var expect = require('chai').expect;
+var nock   = require('nock');
+var Client = require('../src/client');
+
+describe('Person', function () {
+
+  var client = new Client({key: 'k'});
+  var Person = require('../src/person')(client);
+
+  var mock;
+  before(function () {
+    mock = nock('https://person.clearbit.co');
+  });
+  after(nock.cleanAll);
+  afterEach(function () {
+    mock.done();
+  });
+
+  describe('Person#find', function () {
+
+    it('can find a person by email', function () {
+      var alex = require('./fixtures/person');
+      mock
+        .get('/v1/people/email/alex@alexmaccaw.com')
+        .reply(200, alex);
+      return Person.find({email: 'alex@alexmaccaw.com'})
+        .then(function (person) {
+          expect(person)
+            .to.be.an.instanceOf(Person)
+            .and.have.property('id', alex.id);
+        });
+    });
+
+  });
+
+});
