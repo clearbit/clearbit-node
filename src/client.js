c0d2af947fdbdb4b9e5b422007b09cb7f3ee54c4
Ben Drucker <bvdrucker@gmail.com>
2014-10-09 16:02:45 +0200

    Generate the base URL for request using client#base

--- src/client.js
+++ src/client.js
@@ -1,12 +1,29 @@
 'use strict';
 
 var assert = require('assert');
+var util   = require('util');
+var _      = require('lodash');
 
 function ClearbitClient (config) {
   config = config || {};
   assert(this instanceof ClearbitClient, 'Client must be called with new');
   assert(!!config.key, 'An API key must be provided');
   this.key = config.key;
+};
+
+var base = 'https://%s%s.clearbit.co/v%s';
+ClearbitClient.prototype.base = function (options) {
+  options = _.defaults(options || {}, {
+    version: '1',
+    stream: false
+  });
+  assert(options.api, 'An API must be specified');
+  return util.format.apply(util, [
+    base,
+    options.api,
+    options.stream ? '-stream' : '',
+    options.version
+  ]);
 };
 
 module.exports = ClearbitClient;
--- test/client.js
+++ test/client.js
@@ -5,19 +5,61 @@ var ClearbitClient = require('../src/client');
 
 describe('ClearbitClient', function () {
 
-  it('must be called with new', function () {
-    expect(ClearbitClient).to.throw(/called with new/);
+  var client;
+  beforeEach(function () {
+    client = new ClearbitClient({
+      key: 'k'
+    });
   });
 
-  it('must provide an API key', function () {
-    expect(function () {
-      return new ClearbitClient();
-    })
-    .to.throw(/API key/);
+  describe('Constructor', function () {
+
+    it('must be called with new', function () {
+      expect(ClearbitClient).to.throw(/called with new/);
+    });
+
+    it('must provide an API key', function () {
+      expect(function () {
+        return new ClearbitClient();
+      })
+      .to.throw(/API key/);
+    });
+
+    it('configures the API key', function () {
+      expect(new ClearbitClient({key: 'k'})).to.have.property('key', 'k');
+    });
+
   });
 
-  it('configures the API key', function () {
-    expect(new ClearbitClient({key: 'k'})).to.have.property('key', 'k');
+  describe('#base', function () {
+
+    it('requires an API', function () {
+      expect(client.base).to.throw(/API must be specified/);
+    });
+
+    it('can generate the default base', function () {
+      expect(client.base({
+        api: 'person'
+      }))
+      .to.equal('https://person.clearbit.co/v1');
+    });
+
+    it('can generate a streaming base', function () {
+      expect(client.base({
+        api: 'person',
+        stream: true
+      }))
+      .to.equal('https://person-stream.clearbit.co/v1');
+    });
+
+    it('can set a custom version', function () {
+      expect(client.base({
+        api: 'person',
+        version: '2'
+      }))
+      .to.equal('https://person.clearbit.co/v2');
+    });
+
   });
 
 });
