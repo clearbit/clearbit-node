732b660e5afb27d78bdfaab2876b45e046eef5bc
Alex MacCaw <alex@alexmaccaw.com>
2014-12-21 16:06:51 -0700

    add flag API to companies

--- src/company.js
+++ src/company.js
@@ -9,4 +9,13 @@ module.exports = resource.create('Company', {
 })
 .on('preFind', function (options) {
   assert(options.domain, 'A domain must be provided');
+}).include({
+  flag: function(params, options){
+    return this.client.request(_.extend({
+      api: this._options.api,
+      method: 'post',
+      path: _.template('/people/<%= id %>/flag', this),
+      query: params || {}
+    }, options));
+  }
 });
